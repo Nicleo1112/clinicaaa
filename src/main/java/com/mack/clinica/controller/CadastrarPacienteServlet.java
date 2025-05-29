192e89def7ec22b46bbb83ac2568330afaab5c54
Gabriel-Apolinario01 <107645458+Gabriel-Apolinario01@users.noreply.github.com>
2025-05-29 20:12:52 -0300

    Update CadastrarPacienteServlet.java

--- src/main/java/com/mack/clinica/controller/CadastrarPacienteServlet.java
+++ src/main/java/com/mack/clinica/controller/CadastrarPacienteServlet.java
@@ -1,5 +1,7 @@
+// Pacote do controlador
 package com.mack.clinica.controller;
 
+// Importações necessárias
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
@@ -8,10 +10,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/cadastrarPaciente")
+@WebServlet("/cadastrarPaciente") // Mapeia o servlet para a URL /cadastrarPaciente
 public class CadastrarPacienteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Encaminha para a página de cadastro de paciente
         request.getRequestDispatcher("/cadastrarPaciente.jsp").forward(request, response);
     }
 }
