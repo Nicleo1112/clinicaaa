dd8b4a98298d68cd7797c38ee7fb1156277ff6f9
Gabriel-Apolinario01 <107645458+Gabriel-Apolinario01@users.noreply.github.com>
2025-05-30 04:09:39 -0300

    Atualizar o CadastrarMedicoServlet.java

--- src/main/java/com/mack/clinica/controller/CadastrarMedicoServlet.java
+++ src/main/java/com/mack/clinica/controller/CadastrarMedicoServlet.java
@@ -12,9 +12,13 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/cadastrarMedico") // Mapeia o servlet para a URL /cadastrarMedico
 public class CadastrarMedicoServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Encaminha para a página de cadastro de médico
+        // Redireciona a requisição do cliente para o JSP responsável por exibir o formulário de cadastro de médico
         request.getRequestDispatcher("/cadastrarMedico.jsp").forward(request, response);
+        // O método forward mantém a URL original e envia a requisição e resposta para a página JSP,
+        // possibilitando que esta utilize dados do request, se necessário
     }
 }
